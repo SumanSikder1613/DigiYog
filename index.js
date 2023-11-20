aedf6babe6c5f573af002e008171e4ef62d2b5ff
SumanSikder1613 <140959814+SumanSikder1613@users.noreply.github.com>
2023-11-20 17:46:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+ $(document).ready(function(){
+$(".sub-btn").click(function(){
+$(this).next(".sub-menu").slideToggle();
+});
+
+$(".more-btn").click(function(){
+$(this).next(".more-menu").slideToggle();
+    });
+            
+ });
+
+ var menu= document.querySelector(".menu");
+ var menuBtn= document.querySelector(".menu-btn");
+ var closeBtn= document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", ()=>{
+menu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", ()=>{
+ menu.classList.remove("active");
+ });
+
+ function show(anything){
+    document.querySelector('.textbox').value= anything;
+          }
+          
+   let dropdown = document.querySelector('.dropdown');
+   dropdown.onclick= function(){
+   dropdown.classList.toggle('active');
+          }
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+
+let counter = 0;
+
+function updateSlide() {
+    const size = slides[0].clientWidth;
+    slider.style.transform = `translateX(${-size * counter}px)`;
+}
+
+function slideNext() {
+    if (counter >= slides.length - 1){
+        counter=0;
+    } else{
+    counter++;
+    }
+    updateSlide();
+}
+
+function slidePrev() {
+    if (counter <= 0) {
+        counter= slide.length -1;
+    } else {
+    counter--;
+    }
+    updateSlide();
+}
+
+// Automatic slide change every 3 seconds
+setInterval(slideNext, 3000);
